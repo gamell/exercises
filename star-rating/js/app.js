0ba5b9d2f3130d48d8ac1f5366629020b8daba35
Joan Gamell <gamell@gamell.io>
2018-04-15 15:35:30 -0700

    star rating supporting several instances per page

--- star-rating/js/app.js
+++ star-rating/js/app.js
@@ -1,46 +1,58 @@
-const starRating = (function(){
+const starRating = (function() {
 
-  let RATINGS = [5, 4, 3, 2, 1];
-
-  let currRating = 0;
-  let onRating = (_) => {};
-
-  function handleClick(e) {
-    const rating = parseInt(e.target.dataset.rating, 10);
-    if (rating) {
-      // clicking on the same star resets rating to 0
-      currRating = (rating === currRating) ? 0 : rating;
-      onRating(currRating);
-      render($container, currRating);
-    }
-    e.preventDefault();
-  }
-
-  function setEventHandler($container) {
-    $container.addEventListener('click', handleClick);
-  }
-
-  function render($container, rating) {
-    $container.innerHTML = RATINGS.map((currRating) =>
-      `<li class="star star-${currRating <= rating ? 'full' : 'empty'}" data-rating="${currRating}"></li>`
-    ).join('');
-  }
+  const RATINGS = [5, 4, 3, 2, 1];
 
   function init(containerId, initRating = 0) {
-    currRating = initRating;
-    $container = document.getElementById(containerId);
-    render($container);
-    setEventHandler($container)
+    return (function() {
+
+      let currRating = 0;
+      let onRatingHandler = (_) => {};
+      let $container = null;
+
+      function handleClick(e) {
+        const rating = parseInt(e.target.dataset.rating, 10);
+        if (rating) {
+          // clicking on the same star resets rating to 0
+          currRating = (rating === currRating) ? 0 : rating;
+          onRatingHandler(currRating);
+          render($container, currRating);
+        }
+        e.preventDefault();
+      }
+
+      function setEventHandler($container) {
+        $container.addEventListener('click', handleClick);
+      }
+
+      function render($container, rating) {
+        $container.innerHTML =
+        `<ul class="star-rating">` +
+          RATINGS.map((currRating) =>
+            `<li class="star star-${currRating <= rating ? 'full' : 'empty'}" data-rating="${currRating}"></li>`
+          ).join('') +
+        `</ul>`;
+      }
+
+      function onRating(f) {
+        onRatingHandler = f;
+        return api;
+      }
+
+      const api = {
+        onRating
+      }
+
+      currRating = initRating;
+      $container = document.getElementById(containerId);
+      render($container);
+      setEventHandler($container);
+
+      return api;
+
+    })();
   }
 
-  function setOnRating(f) {
-    onRating = f;
-  }
-
-  return {
-    init,
-    setOnRating
-  }
+  return init;
 
 })();
 
@@ -49,5 +61,8 @@ function onRating(rating) {
   $message.innerHTML = `Thank you for your ${rating}-star rating!`;
 }
 
-starRating.setOnRating(onRating);
-window.onload = () => starRating.init('star-rating');
+window.onload = () => {
+  const starRating1 = starRating('star-rating-1');
+  const starRating2 = starRating('star-rating-2');
+  starRating2.onRating(onRating);
+};
